566e8c59405e518c6c7cdd1851efa980aa4f0798
chuks_nlog <amaechichuks2000@yahoo.com>
2018-10-26 13:43:10 +0100

    code refactor

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
         })
-        .catch(console.log)
+        //.catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
         
